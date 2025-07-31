7b7378c9aa1ca320b5236183ed507694567e0498
davithekid <byjdrk.27@gmail.com>
2025-07-31 11:13:29 -0300

    feat: mysql2

--- back-end/configs/database.js
+++ back-end/configs/database.js
@@ -1,17 +1,14 @@
 import knex from 'knex';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 class Database {
     constructor() {
         this.connection = knex({
-            client: 'sqlite3',
+            client: 'mysql2',
             connection: {
-                filename: path.resolve(__dirname, 'db.sqlite')
+                host: 'localhost',       
+                user: 'root',             
+                password: '',    
+                database: 'sistema_chamados'
             },
             useNullAsDefault: true,
         });
