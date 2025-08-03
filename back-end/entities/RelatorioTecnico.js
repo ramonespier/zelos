a0fd88ab8bb66d23784f90eae49b882fe46067b6
davithekid <byjdrk.27@gmail.com>
2025-08-03 15:36:32 -0300

    fix: relatorio tecnnico

--- back-end/entities/RelatorioTecnico.js
+++ back-end/entities/RelatorioTecnico.js
@@ -4,22 +4,36 @@ import sequelize from "../configs/database.js";
 class RelatorioTecnico extends Model {}
 
 RelatorioTecnico.init({
-    nome_tecnico:{
+    nome_tecnico: {
         type: DataTypes.STRING(255),
+        allowNull: true,
     },
     tecnico_id: {
         type: DataTypes.UUID,
+        allowNull: true,
     },
-    total_chamados:{
+    total_chamados: {
         type: DataTypes.INTEGER,
+        allowNull: true,
     },
-    tempo_medio_minutos:{
-        type: DataTypes.INTEGER, 
-    }
-}, {  sequelize,
+    tempo_medio_minutos: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+}, {
+    sequelize,
     modelName: 'RelatorioTecnico',
     tableName: 'vw_atividades_tecnicos',
     timestamps: false,
-  })
+    // 👇 força o Sequelize a aceitar que não há chave primária
+    createdAt: false,
+    updatedAt: false,
+    indexes: [], // impede erro de PK implícita
+    defaultScope: {
+        attributes: {
+            exclude: ['id'] // garante que ele não tente buscar um campo id
+        }
+    }
+});
 
 export default RelatorioTecnico;
